385145e1ddeffc0a5da754a9b8e0b8d70139a93a
jmfigueroah01-creator <jmfigueroah01@gmail.com>
2026-09-19 14:43:11 -0600

    Update URL_VALIDAR_CODIGO with new endpoint

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const DOMINIO_INSTITUCIONAL = "@afac.gob.mx";
 
 // URLs de los dos flujos de Power Automate para el código de acceso.
 const URL_ENVIAR_CODIGO = "https://defaultb7c9bdfffd974461ab1bd2a2813f8b.a4.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/10/workflows/ba842193e68f400998fc6341ccaa467c/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=C3iuWkiPu8P7CBwgNp9rZLmnA2O71bzkBs0tO94wNoA";
-const URL_VALIDAR_CODIGO = "";
+const URL_VALIDAR_CODIGO = "https://defaultb7c9bdfffd974461ab1bd2a2813f8b.a4.environment.api.powerplatform.com:443/powerautomate/automations/direct/cu/26/workflows/99a974f68f4b410697ac1202e178d13d/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=FiEyhSJ-qHuIAB-OcC0EoN3ZxaxDMzXtqr1t2oKs0gI";
 
 // URL del flujo que registra progreso (correo + evento + fecha).
 const WEBHOOK_URL = "";
